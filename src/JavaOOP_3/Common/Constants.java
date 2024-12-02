ccc6e166e3e45c63ab12cf990283d7f4eda63377
ltdung8 <ledung1998hello@gmail.com>
2024-12-02 23:56:16 +0700

    2nd
    OOP

--- src/JavaOOP_3/Common/Constants.java
+++ src/JavaOOP_3/Common/Constants.java
@@ -4,5 +4,6 @@ public class Constants {
     public static void main(String[] args) {
         System.out.println("heh");
         System.out.println("");
+        System.out.println("finally");
     }
 }
